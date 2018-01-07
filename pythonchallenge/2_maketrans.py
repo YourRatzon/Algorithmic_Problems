d109b9c60d6305afd3d5dcfc4eaa85108cb96404
Yechiel Labunskiy <YourRatzon@users.noreply.github.com>
2018-01-08 00:59:58 +0200

    Added a main

--- pythonchallenge/2_maketrans.py
+++ pythonchallenge/2_maketrans.py
@@ -1,7 +1,14 @@
 import string
 
-st = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
-intab = string.lowercase
-outtab = string.lowercase[2:] + "ab"
-trantab = string.maketrans(intab, outtab)
-print (st.translate(trantab))
+def solve(st):
+    intab = string.lowercase
+    outtab = string.lowercase[2:] + "ab"
+    trantab = string.maketrans(intab, outtab)
+    print (st.translate(trantab))
+
+if __name__ == '__main__':
+    st = ("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp."
+          " bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm "
+          "jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.")
+    solve(st)
+    solve("map")
